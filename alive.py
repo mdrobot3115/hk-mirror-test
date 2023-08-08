0557500809a7c85d5136a22480de72793e0e13e2
mdrobot3115 <116418622+mdrobot3115@users.noreply.github.com>
2023-08-08 19:56:29 +0530

    Update alive.py

--- alive.py
+++ alive.py
@@ -1,9 +1,9 @@
 from time import sleep
-from requests import get as zget
+from requests import get as rget
 from os import environ
 from logging import error as logerror
 
-BASE_URL = environ.get('BASE_URL', None)
+BASE_URL = environ.get('BASE_URL_HK', None)
 try:
     if len(BASE_URL) == 0:
         raise TypeError
@@ -14,8 +14,8 @@ PORT = environ.get('PORT', None)
 if PORT is not None and BASE_URL is not None:
     while True:
         try:
-            zget(BASE_URL).status_code
-            sleep(300)
+            rget(BASE_URL).status_code
+            sleep(600)
         except Exception as e:
             logerror(f"alive.py: {e}")
             sleep(2)
